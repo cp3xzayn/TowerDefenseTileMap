bc453d0b23c7f248f1e52bc5ab8d09db99c0fb60
h.kato <cp3xzayn@gmail.com>
2020-11-01 06:29:27 +0900

    Astar用にMapの配列を二次元配列に定義しなおした。

--- Assets/Scripts/Astar.cs
+++ Assets/Scripts/Astar.cs
@@ -19,6 +19,20 @@ public class Astar : MonoBehaviour
     /// <summary>スタートのタイル/ </summary>
     Tile[] m_goalTile;
 
+    //それぞれのTileの状態
+    enum NodState
+    {
+        None,
+        Open,
+        Close
+    }
+    NodState nodState;
+    /// <summary>実コスト/ </summary>
+    int cost;
+    /// <summary>推定コスト/ </summary>
+    int estimatedCost;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,47 +50,42 @@ public class Astar : MonoBehaviour
     {
         
     }
+
     void RoomMaping(Tile wallTile, Tile roadTile, Tile startTile, Tile goalTile, Vector3Int position, int roomX, int roomY)
     {
-        int tileIndex = roomX * roomY;
         //roomX×roomYの部屋を想定(xの横並びで考える)
-        Tile[] tile = new Tile[tileIndex];
+        Tile[,] tile = new Tile[roomX, roomY];
         //置くtileを配列に収納
-        for (int i = 0; i < tile.Length; i++)
+        for (int i = 0; i < roomX; i++)
         {
-            tile[i] = roadTile;
-        }
-
-        tile[0] = startTile;
-        tile[3] = wallTile;
-        tile[4] = wallTile;
-        tile[9] = wallTile;
-        tile[11] = wallTile;
-        tile[13] = wallTile;
-        tile[17] = wallTile;
-        tile[20] = wallTile;
-        tile[24] = goalTile;
-
-        int putIndex = 0;
-        //配列からtileを置く(xの横並びで考える)
-        for (int y = 0; y < roomY; y++)
-        {
-            for (int x = 0; x < roomX; x++)
+            for (int j = 0; j < roomY; j++)
             {
-                position = new Vector3Int(x, y, 0);
-                m_tilemap.SetTile(position, tile[putIndex]);
-                putIndex++;
+                tile[i, j] = roadTile;
+                position = new Vector3Int(i, j, 0);
+                m_tilemap.SetTile(position, tile[i, j]);
             }
         }
+        //スタートゴール、障害物を配列に格納する
+        tile[0, 0] = startTile;
+        tile[3, 0] = wallTile;
+        tile[4, 0] = wallTile;
+        tile[4, 1] = wallTile;
+        tile[1, 2] = wallTile;
+        tile[3, 2] = wallTile;
+        tile[2, 3] = wallTile;
+        tile[0, 4] = wallTile;
+        tile[4, 4] = goalTile;
 
-        m_tilemap.SetTile(new Vector3Int(0, 0, 0), tile[0]);
-        m_tilemap.SetTile(new Vector3Int(3, 0, 0), tile[3]);
-        m_tilemap.SetTile(new Vector3Int(4, 0, 0), tile[4]);
-        m_tilemap.SetTile(new Vector3Int(4, 1, 0), tile[9]);
-        m_tilemap.SetTile(new Vector3Int(1, 2, 0), tile[11]);
-        m_tilemap.SetTile(new Vector3Int(3, 2, 0), tile[13]);
-        m_tilemap.SetTile(new Vector3Int(2, 3, 0), tile[17]);
-        m_tilemap.SetTile(new Vector3Int(0, 4, 0), tile[20]);
-        m_tilemap.SetTile(new Vector3Int(4, 4, 0), tile[24]);
+        //スタート、ゴール、障害物をマップに配置
+        m_tilemap.SetTile(new Vector3Int(0, 0, 0), tile[0, 0]);
+        m_tilemap.SetTile(new Vector3Int(3, 0, 0), tile[3, 0]);
+        m_tilemap.SetTile(new Vector3Int(4, 0, 0), tile[4, 0]);
+        m_tilemap.SetTile(new Vector3Int(4, 1, 0), tile[4, 1]);
+        m_tilemap.SetTile(new Vector3Int(1, 2, 0), tile[1, 2]);
+        m_tilemap.SetTile(new Vector3Int(3, 2, 0), tile[3, 2]);
+        m_tilemap.SetTile(new Vector3Int(2, 3, 0), tile[2, 3]);
+        m_tilemap.SetTile(new Vector3Int(0, 4, 0), tile[0, 4]);
+        m_tilemap.SetTile(new Vector3Int(4, 4, 0), tile[4, 4]);
+
     }
 }
