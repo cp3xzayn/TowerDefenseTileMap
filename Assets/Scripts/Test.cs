86ad10baea66ea9078d6ed0a44246e1b8170b2ee
h.kato <cp3xzayn@gmail.com>
2020-11-05 13:42:53 +0900

    13×13のマップを作成した。Paletteを整理した。

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,24 +4,28 @@ using UnityEngine.Tilemaps;
 public class Test : MonoBehaviour
 {
     [SerializeField] Tilemap m_tilemap;
-    /// <summary>壁のタイル</summary>
-    Tile[] m_wallTile;
-    /// <summary>道のタイル</summary>
-    Tile[] m_roadTile;
-    /// <summary>スタートのタイル/ </summary>
+    /// <summary>敵のスタート位置のタイル/ </summary>
     Tile[] m_startTile;
-    /// <summary>スタートのタイル/ </summary>
-    Tile[] m_goalTile;
+    /// <summary>敵の歩く道のタイル</summary>
+    Tile[] m_enemyRoadTile;
+    /// <summary>Playerの歩く道のタイル </summary>
+    Tile[] m_prayerRoadTile;
+    /// <summary>拠点のタイル </summary>
+    Tile[] m_hubTile;
+    /// <summary>障害物のタイル</summary>
+    Tile[] m_wallTile;
     // Start is called before the first frame update
     void Start()
     {
         //Resourcesフォルダーからタイルを読み込む
-        m_roadTile = Resources.LoadAll<Tile>("RoadPalette");
-        m_wallTile = Resources.LoadAll<Tile>("WallPalette");
         m_startTile = Resources.LoadAll<Tile>("StartPalette");
-        m_goalTile = Resources.LoadAll<Tile>("GoalPalette");
+        m_enemyRoadTile = Resources.LoadAll<Tile>("EnemyRoadPalette");
+        m_prayerRoadTile = Resources.LoadAll<Tile>("PlayerRoadPalette");
+        m_hubTile = Resources.LoadAll<Tile>("HubPalette");
+        m_wallTile = Resources.LoadAll<Tile>("WallPalette");
+        
         Vector3Int m_vector3Int = new Vector3Int(0, 0, 0);
-        TestMap(m_wallTile[0], m_roadTile[0], m_startTile[0], m_goalTile[0], m_vector3Int);
+        TestMap(m_startTile[0], m_enemyRoadTile[0], m_prayerRoadTile[0], m_hubTile[0], m_wallTile[0], m_vector3Int);
     }
 
     // Update is called once per frame
@@ -29,35 +33,46 @@ public class Test : MonoBehaviour
     {
 
     }
-    void TestMap(Tile w, Tile r, Tile s, Tile g, Vector3Int position)
+    void TestMap(Tile es ,Tile er, Tile pr, Tile hu, Tile ob, Vector3Int position)
     {
         //mapを配列で定義
-        int[,] map = new int[5, 5]{
-            { 0, 2, 2, 2, 3},
-            { 2, 2, 3, 2, 2},
-            { 2, 2, 2, 3, 2},
-            { 3, 2, 3, 2, 2},
-            { 3, 3, 2, 2, 1}
+        int[,] map = new int[13, 13]{
+            {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 0},
+            {1, 1, 1, 2, 4, 2, 4, 2, 2, 2, 1, 2, 2},
+            {2, 2, 1, 2, 4, 2, 2, 4, 2, 1, 1, 2, 4},
+            {2, 2, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2},
+            {2, 2, 1, 1, 1, 1, 2, 1, 1, 1, 2, 4, 2},
+            {2, 2, 2, 2, 2, 3, 3, 3, 4, 2, 2, 2, 2},
+            {2, 2, 4, 2, 2, 3, 3, 3, 2, 2, 4, 2, 4},
+            {2, 2, 2, 2, 4, 3, 3, 3, 2, 4, 2, 2, 2},
+            {2, 4, 2, 2, 1, 1, 2, 1, 2, 2, 2, 4, 2},
+            {2, 2, 2, 1, 1, 2, 2, 1, 2, 4, 2, 2, 2},
+            {2, 2, 2, 1, 4, 2, 4, 1, 2, 2, 2, 2, 2},
+            {2, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 4, 2},
+            {0, 1, 4, 2, 2, 4, 2, 2, 2, 4, 1, 1, 0}
         };
         //mapの配列にTileを配置
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 13; i++)
         {
-            for (int j = 0; j < 5; j++)
+            for (int j = 0; j < 13; j++)
             {
                 position = new Vector3Int(i, j, 0);
                 switch (map[i, j])
                 {
                     case 0:
-                        m_tilemap.SetTile(position, s);
+                        m_tilemap.SetTile(position, es);
                         break;
                     case 1:
-                        m_tilemap.SetTile(position, g);
+                        m_tilemap.SetTile(position, er);
                         break;
                     case 2:
-                        m_tilemap.SetTile(position, r);
+                        m_tilemap.SetTile(position, pr);
                         break;
                     case 3:
-                        m_tilemap.SetTile(position, w);
+                        m_tilemap.SetTile(position, hu);
+                        break;
+                    case 4:
+                        m_tilemap.SetTile(position, ob);
                         break;
                 }
             }
