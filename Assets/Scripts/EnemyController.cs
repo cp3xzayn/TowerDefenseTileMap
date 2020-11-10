b5acdef1020685b15e11b8d9ef6e44dde55c7bac
h.kato <cp3xzayn@gmail.com>
2020-11-11 01:55:36 +0900

    敵が生成され、拠点までたどり着くようにした。（m[0, 0]に生成された場合のみ）

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,7 +7,7 @@ public class EnemyController : MonoBehaviour
     GameObject m_mapGene;
     GameObject m_eneGene;
     //敵の進むスピード
-    [SerializeField] float m_move;
+    [SerializeField] float m_move = 0.2f;
     SpriteRenderer m_sprite;
     Animator m_anim;
     Rigidbody2D m_rb;
@@ -52,16 +52,7 @@ public class EnemyController : MonoBehaviour
             //x方向に道があるとき、移動する
             if (nextX == 1)
             {
-                m_move = 0.1f;
                 m_rb.velocity = new Vector3(m_move, 0, 0);
-                //次のタイルに進み終わったら移動を終了する
-                if (m_enePos.x >= nextPosX.x)
-                {
-                    m_move = 0;
-                    m_rb.velocity = new Vector3(m_move, 0, 0);
-                    x = x + 1;//配列を次のタイルに進ませる
-                    Debug.Log("x:" + x + "y:" + y);
-                }
             }
         }
         //敵が道にいるとき
@@ -72,32 +63,28 @@ public class EnemyController : MonoBehaviour
             //x方向に道があるとき、移動する
             if (nextX == 1)
             {
-                m_move = 0.1f;
                 m_rb.velocity = new Vector3(m_move, 0, 0);
-                //次のタイルに進み終わったら移動を終了する
-                if (m_enePos.x >= nextPosX.x)
-                {
-                    m_move = 0f;
-                    m_rb.velocity = new Vector3(m_move, 0, 0);
-                    x = x + 1;//配列を次のタイルに進ませる
-                    Debug.Log("x:" + x + "y:" + y);
-                }
             }
-
             //y方向に道があるとき、移動する
             if (nextY == 1)
             {
-                m_move = 0.1f;
                 m_rb.velocity = new Vector3(0, m_move, 0);
-                //次のタイルに進み終わったら移動を終了する
-                if (m_enePos.y >= nextPosY.y)
-                {
-                    m_move = 0;
-                    m_rb.velocity = new Vector3(0, m_move, 0);
-                    y = y + 1;//配列を次のタイルに進ませる
-                    Debug.Log("x:" + x + "y:" + y);
-                }
             }
+            //x方向に拠点があるとき、移動する
+            if (nextX == 3)
+            {
+                m_rb.velocity = new Vector3(m_move, 0, 0);
+            }
+            //y方向に拠点があるとき、移動する
+            if (nextY == 3)
+            {
+                m_rb.velocity = new Vector3(0, m_move, 0);
+            }
+        }
+        //敵が拠点にたどり着いたら行動を止める
+        if (m[x, y] == 3)
+        {
+            m_rb.velocity = new Vector3(0, 0, 0);
         }
     }
 }
