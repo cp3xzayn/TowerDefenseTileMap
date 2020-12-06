84776aff64fbba4aefc15538d205283b834b68f7
h.kato <cp3xzayn@gmail.com>
2020-12-06 12:54:43 +0900

    敵が射程範囲内にある時に弾を発射するようにした（改善必要）

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -1,42 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
 
 public class WeaponManager : MonoBehaviour
 {
-    [SerializeField]GameObject m_bullet;
-    private GameObject m_enemy;
+    [SerializeField] GameObject m_bullet;
+    private GameObject[] m_enemy;
     /// <summary>弾の生成ポジション</summary>
     private Vector3 m_startPosition;
     /// <summary>敵のポジション</summary>
-    private Vector3 m_goalPosition;
+    private Vector3[] m_goalPosition;
+    private Vector3 m_enemyPosition;
     /// <summary>二点間の距離/summary>
     private float m_distance;
     [SerializeField] float m_limitRange = 4.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
+        OnShot();
     }
+
+    //ボタンが押されたときに（仮）
     public void OnShot()
     {
         //弾と敵のポジションを取得する
         m_startPosition = this.transform.position;
-        m_enemy = GameObject.FindGameObjectWithTag("Enemy");
-        m_goalPosition = m_enemy.transform.position;
-        //二点間の距離を代入
-        m_distance = Vector2.Distance(m_startPosition, m_goalPosition);
-        Debug.Log(m_distance);
-        if (m_distance <= m_limitRange)
+        m_enemy = GameObject.FindGameObjectsWithTag("Enemy");
+        m_goalPosition = new Vector3[m_enemy.Length];
+        for (int i = 0; i < m_enemy.Length; i++)
         {
-            Instantiate(m_bullet, this.transform.position, Quaternion.identity);
+            m_goalPosition[i] = m_enemy[i].transform.position;
+            // 二点間の距離を代入
+            m_distance = Vector3.Distance(m_startPosition, m_goalPosition[i]);
+            Debug.Log(m_distance);
+            if (m_distance <= m_limitRange)
+            {
+                m_enemyPosition = m_goalPosition[i];
+                Instantiate(m_bullet, this.transform.position, Quaternion.identity);
+            }
         }
-        
+
+    }
+
+    public Vector3 EnemyPos
+    {
+        get { return m_enemyPosition; }
     }
 }
+ 
