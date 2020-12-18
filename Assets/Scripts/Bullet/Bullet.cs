858f70158b3b868466e2abf78afd342994aa0c0e
h.kato <cp3xzayn@gmail.com>
2020-12-18 23:22:00 +0900

    BulletControllerを作成し、弾の種類を増やすための基盤を作成した。

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    int bulletDamage;
+    float bulletRange;
+
+    public float BulletRange { get { return bulletRange; } }
+
+    /// <summary>
+    /// 弾のステータスをセットする関数
+    /// </summary>
+    /// <param name="bDamage"></param>
+    /// <param name="bRange"></param>
+    public void SetBullet(int bDamage, float bRange)
+    {
+        bulletDamage = bDamage;
+        bulletRange = bRange;
+    }
+}
