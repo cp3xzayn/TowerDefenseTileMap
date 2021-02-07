6c73ea9ef96df0b4cd1c7b705447d0c2bc5dd8ae
h.kato <cp3xzayn@gmail.com>
2021-02-08 02:01:32 +0900

    作成したUIをマウスカーソルが合ったときに表示するようにした

---
+++ Assets/Scripts/MousePointer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePointer : MonoBehaviour
+{
+    /// <summary> MouseカーソルのＸ座標 </summary>
+    float m_mouseX;
+    /// <summary> MouseカーソルのＸ座標 </summary>
+    float m_mouseY;
+
+    [SerializeField] GameObject m_weapon1Status;
+    [SerializeField] GameObject m_weapon2Status;
+
+    void Update()
+    {
+        WeaponStatusVisible();
+    }
+
+    void WeaponStatusVisible()
+    {
+        m_mouseX = Input.mousePosition.x;
+        m_mouseY = Input.mousePosition.y;
+
+        if (m_mouseX > 30 && m_mouseX < 65 && m_mouseY > 360 && m_mouseY < 405)
+        {
+            m_weapon1Status.SetActive(true);
+        }
+        else
+        {
+            m_weapon1Status.SetActive(false);
+        }
+
+        if (m_mouseX > 30 && m_mouseX < 65 && m_mouseY > 232 && m_mouseY < 270)
+        {
+            m_weapon2Status.SetActive(true);
+        }
+        else
+        {
+            m_weapon2Status.SetActive(false);
+        }
+    }
+}
