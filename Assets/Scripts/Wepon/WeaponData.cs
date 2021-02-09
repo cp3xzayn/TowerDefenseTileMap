c90b4457b37ec5c3a370d6c0f770592965d73ef5
h.kato <cp3xzayn@gmail.com>
2021-02-10 05:34:57 +0900

    WeaponDataのJsonファイルを作成し、取得できるようにした

--- Assets/Scripts/Wepon/WeaponData.cs
+++ Assets/Scripts/Wepon/WeaponData.cs
@@ -1,10 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+[Serializable]
+public class InputJsonWeaponData
+{
+    public float[] m_weaponData;
+}
 
 public class WeaponData : MonoBehaviour
 {
     [SerializeField] float m_shootTime = 2f;
+
+    int m_index;
+    float m_shootingTime;
+
+    public float LoadWeaponData(int index)
+    {
+        m_index = index;
+        string inputString = Resources.Load<TextAsset>("Json/WeaponData").ToString();
+        InputJsonWeaponData inputJsonWeaponData = JsonUtility.FromJson<InputJsonWeaponData>(inputString);
+        m_shootingTime = inputJsonWeaponData.m_weaponData[m_index];
+        return m_shootingTime;
+    }
+
 
     public float ShootTime { get { return m_shootTime; } }
 
@@ -14,5 +34,10 @@ public class WeaponData : MonoBehaviour
     {
         wMana = GetComponent<WeaponManager>();
         wMana.SetWeaponData(m_shootTime);
+    }
+
+    void OnClickWeaponStren()
+    {
+
     }
 }
