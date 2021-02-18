99da864d1c50463a2e302865f9559433c3769c01
cp3xzayn <cp3xzayn@gmail.com>
2021-02-18 22:39:21 +0900

    WaveTimeと準備時間のTextをどちらかしか表示しないようにした

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -42,6 +42,7 @@ public class GameManager : MonoBehaviour
     Text m_waveText;
     /// <summary> Waveクリア時に表示するText </summary>
     [SerializeField] Text m_waveClearText;
+    [SerializeField] GameObject m_waveTimeObject;
     /// <summary> WaveTimeを表示するText </summary>
     [SerializeField] Text m_waveTimeText;
 
@@ -231,6 +232,8 @@ public class GameManager : MonoBehaviour
         if (m_preparationTime < 0)
         {
             m_preTimeText.text = "じゅんびじかん : 0.0";
+            m_preTimeObject.SetActive(false);
+            m_waveTimeObject.SetActive(true);
             //Battleに変更する
             SetNowState(GameState.Battle);
         }
