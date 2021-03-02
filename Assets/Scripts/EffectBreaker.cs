71f77c18b877d622456c179d365ddaefac6a59da
h.kato <cp3xzayn@gmail.com>
2021-03-03 04:34:44 +0900

    敵死亡時のエフェクトを破壊できるようにした

---
+++ Assets/Scripts/EffectBreaker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectBreaker : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("BreakEffect", 2f);
+    }
+
+    void BreakEffect()
+    {
+        Destroy(this.gameObject);
+    }
+}
